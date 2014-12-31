18c87737393f26f0b92347e65bc10e5b6c105e4a
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2014-12-31 14:28:54 +0000

    copy of bitbucket repository now sync'ed with git as well

---
+++ src/main/java/org/xmlcml/nhtml/document/hindawi/HindawiReader.java
@@ -0,0 +1,16 @@
+package org.xmlcml.nhtml.document.hindawi;
+
+import org.xmlcml.nhtml.InputType;
+import org.xmlcml.nhtml.document.DocumentReader;
+
+public class HindawiReader extends DocumentReader {
+
+	 HindawiReader() {
+		super();
+	}
+
+	public HindawiReader(InputType type) {
+		super(type);
+	}
+
+}
